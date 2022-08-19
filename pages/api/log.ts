1da0bdc5062208bffddc68472d74e26f7f445cfa
Adrian Burgos <adrian.burgos.colas@gmail.com>
2022-08-19 14:35:49 -0400

    added Layout component

--- pages/api/log.ts
+++ pages/api/log.ts
@@ -1,6 +1,13 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import Cors from 'cors'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import Cors from 'cors';
+import { Telegraf, Context } from 'telegraf';
+import {Update} from 'typegram';
+import dotenv from "dotenv";
+dotenv.config();
 
+const bot: Telegraf<Context<Update>> = new Telegraf(process.env.BOT_TOKEN as string);
+
+const chatId: number = 982089486;
 // Initializing the cors middleware
 // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
 const cors = Cors({
@@ -31,7 +38,6 @@ export default async function handler(
 ) {
   // Run the middleware
   await runMiddleware(req, res, cors)
-
-  // Rest of the API logic
+  bot.telegram.sendMessage(chatId, "Hello");
   res.json({ message: 'Hello Everyone!' })
 }
